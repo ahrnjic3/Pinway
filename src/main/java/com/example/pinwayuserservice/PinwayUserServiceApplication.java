1b257f32247eaaa6fd676ccbff68cb683fb9694b
ahrnjic3 <72994482+ahrnjic3@users.noreply.github.com>
2023-03-22 20:57:33 +0100

    Initial commit

---
+++ src/main/java/com/example/pinwayuserservice/PinwayUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinwayuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinwayUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinwayUserServiceApplication.class, args);
+    }
+
+}
