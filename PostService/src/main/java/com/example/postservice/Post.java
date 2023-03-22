2c684f68554898d196a2d8077c93679abe9d5d2c
ahrnjic3 <72994482+ahrnjic3@users.noreply.github.com>
2023-03-22 23:33:38 +0100

    Added PostService Controller

---
+++ PostService/src/main/java/com/example/postservice/Post.java
@@ -0,0 +1,71 @@
+package com.example.postservice;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Post {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    private String image_path;
+
+    private Integer user_id;
+
+    private Integer pin_counter;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage_path() {
+        return image_path;
+    }
+
+    public void setImage_path(String image_path) {
+        this.image_path = image_path;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public Integer getPin_counter() {
+        return pin_counter;
+    }
+
+    public void setPin_counter(Integer pin_counter) {
+        this.pin_counter = pin_counter;
+    }
+}
