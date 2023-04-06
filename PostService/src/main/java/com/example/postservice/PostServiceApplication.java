cbfb3fabc09e35797464cd94ff2f2afbcb5b7d77
ahrnjic3 <72994482+ahrnjic3@users.noreply.github.com>
2023-04-06 10:42:50 +0200

    Added EurekaServer application

--- PostService/src/main/java/com/example/postservice/PostServiceApplication.java
+++ PostService/src/main/java/com/example/postservice/PostServiceApplication.java
@@ -4,9 +4,14 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
