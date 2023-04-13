e05b22484cffd04dd2a5bf96b046bcc83af63091
ahrnjic3 <72994482+ahrnjic3@users.noreply.github.com>
2023-04-13 11:18:39 +0200

    Added configer server settings for post Service.

--- PostService/src/main/java/com/example/postservice/PostServiceApplication.java
+++ PostService/src/main/java/com/example/postservice/PostServiceApplication.java
@@ -1,23 +1,14 @@
 package com.example.postservice;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @SpringBootApplication
 @OpenAPIDefinition
 public class PostServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PostServiceApplication.class, args);
     }
