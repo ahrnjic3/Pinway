bceb34abb8d041b3819a9d922ef6710bc6b561a3
ahrnjic3 <72994482+ahrnjic3@users.noreply.github.com>
2023-05-14 00:37:08 +0200

    merged system events and synchronous communication branches

--- PostService/src/main/java/com/example/postservice/PostServiceApplication.java
+++ PostService/src/main/java/com/example/postservice/PostServiceApplication.java
@@ -1,11 +1,14 @@
 package com.example.postservice;
 
+import interceptor.SystemEventHandlerInterceptor;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -19,5 +22,19 @@ public class PostServiceApplication {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public SystemEventHandlerInterceptor myCustomHandlerInterceptor() {
+        return new SystemEventHandlerInterceptor();
+    }
+    @Bean
+    public WebMvcConfigurer adapter() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(myCustomHandlerInterceptor());
+            }
+        };
     }
 }
