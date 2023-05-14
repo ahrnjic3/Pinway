668d8c2a66415be5ebfb9fab1fb9d75971ddfb92
ahrnjic3 <72994482+ahrnjic3@users.noreply.github.com>
2023-05-14 14:46:47 +0200

    added system events logging to the rest of the services

--- NotificationService/src/main/java/com/example/notificationservice/NotificationServiceApplication.java
+++ NotificationService/src/main/java/com/example/notificationservice/NotificationServiceApplication.java
@@ -1,9 +1,13 @@
 package com.example.notificationservice;
 
 
+import interceptor.SystemEventHandlerInterceptor;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @SpringBootApplication
@@ -13,4 +17,17 @@ public class NotificationServiceApplication {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
 
+    @Bean
+    public SystemEventHandlerInterceptor myCustomHandlerInterceptor() {
+        return new SystemEventHandlerInterceptor();
+    }
+    @Bean
+    public WebMvcConfigurer adapter() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(myCustomHandlerInterceptor());
+            }
+        };
+    }
 }
--- UserService/src/main/java/com/example/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,11 +1,14 @@
 package com.example.userservice;
 
+import interceptor.SystemEventHandlerInterceptor;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @SpringBootApplication
@@ -20,6 +23,20 @@ public class UserServiceApplication {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public SystemEventHandlerInterceptor myCustomHandlerInterceptor() {
+        return new SystemEventHandlerInterceptor();
+    }
+    @Bean
+    public WebMvcConfigurer adapter() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                registry.addInterceptor(myCustomHandlerInterceptor());
+            }
+        };
     }
 
 }
