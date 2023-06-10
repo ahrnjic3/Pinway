4fc31a6e743036c53629075ff65b3278aac6bb7e
ahrnjic3 <72994482+ahrnjic3@users.noreply.github.com>
2023-06-10 15:42:48 +0200

    Added bearer token to all routes on front

--- pinway-frontend/src/api/users.js
+++ pinway-frontend/src/api/users.js
@@ -18,7 +18,7 @@ export const getFollowersForUser = async (id) => {
 };
 
 export const addUserToCollection = async (id, {data}) => {
-  const response = await API.post(`/api/users/${id}/collection`, data, { headers: { Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjpbIlJPTEVfVVNFUiJdLCJzdWIiOiJlYmFzaWMiLCJpYXQiOjE2ODYzMzIyMTUsImV4cCI6MTY4NjY5MjIxNX0.sQuhchdEPaC-H1j1RzFyCtCNWyCjWTYOI0wF3yqBARjwjl4v4xAWGydWs-CulyjxPlLaZeHtZr-RQJU6R6F3Pw` } });
+  const response = await API.post(`/api/users/${id}/collection`, data, { headers: { Authorization: `Bearer ${localStorage.getItem('Bearer')}` } });
   //console.log(response.data);
   return response.data;
 };
